71b5d361d292040c38de828b2de7385742bf8877
Philip Zeyliger <philip@cloudera.com>
2019-02-14 16:46:11 -0800

    CDH-77790: Dynamically figure out Kudu (and other CDH) versions.
    Similar to CDH-72729, we ran into issues with needing to update Impala
    for a Kudu version bump. This uses a properties file that CDH is
    publishing to figure out the versions.
    
    The alternative would have been to strip versions from the tarball files
    themselves, but I think this is a bit more coherent.
    
    Some of the environment variables were removed because
    they're not used. Upstream uses them for impala-parent/pom.xml,
    but downstream we use the cdh root pom.
    
    I tested this by running test-with-docker on this branch as well
    as a private Cauldron build.
    
    Change-Id: I355796180a1bad99cf279c3ef212baef5ecc5a18
    Reviewed-on: https://gerrit.sjc.cloudera.com/41787
    Reviewed-by: Hector Acosta <hector.acosta@cloudera.com>
    Tested-by: Jenkins User <jenkins@cloudera.com>
    CDH-Build: Jenkins User <jenkins@cloudera.com>
    Reviewed-by: Andrew Sherman <asherman@cloudera.com>
    Quasar-L0: Jenkins User <jenkins@cloudera.com>

--- bin/impala-config-branch.sh
+++ bin/impala-config-branch.sh
@@ -19,24 +19,24 @@
 # Config changes for release or features branches should go here so they
 # can be version controlled but not conflict with changes on the master
 # branch.
-#
-# E.g. to override IMPALA_HADOOP_VERSION, you could uncomment this line:
-# IMPALA_HADOOP_VERSION=3.0.0
 
 # To facilitate building and testing across many possible CDH components,
 # we use $CDH_GBN to specify which build we are working against.
 # This is determined as follows.
 # 1. If $GLOBAL_BUILD_NUMBER is set, use that for $CDH_GBN.
-#    In a Cauldron build, this will be set, as well as DOWNLOAD_CDH_COMPONENTS=false.
+#    In a Cauldron build, this will be set, as well as CAULDRON_DOCKER_PLATFORM.
 # 2. If toolchain/cdh_components/cdh-gbn.sh exists, source it to set
 #    $CDH_GBN. This will cause subsequent builds in the same $IMPALA_HOME
-#    to continue using the same cdh components.
+#    to continue using the same cdh components. 
 # 3. Query BuildDB for the latest CDH build tagged with impala-minicluster-tarballs,
-#    and use that.
+#    and use that. The result is cached in cdh-gbn.sh so that a workspace
+#    continues to use the same components.
 # In addition to being used by bin/bootstrap-toolchain.sh to download the
 # tarballs to support the minicluster, CDH_GBN is used in {fe,testdata}/pom.xml
-# to choose a Maven repository with jars from that specific GBN, though this
-# does not currently always work.
+# to choose a Maven repository with jars from that specific GBN.
+#
+# Furthermore, a cdh.properties file matching the GBN is used
+# to figure out the CDH component versions.
 
 # Defer to $GLOBAL_BUILD_NUMBER OR $CDH_GBN, in that order.
 export CDH_GBN=${GLOBAL_BUILD_NUMBER:-${CDH_GBN:-}}
@@ -77,23 +77,38 @@ elif [ -z "$CAULDRON_DOCKER_PLATFORM" ]; then
   export IMPALA_MAVEN_OPTIONS="-s ${MAVEN_CONFIG_FILE}"
 fi
 
-BUILD_REPO_BASE="http://cloudera-build-us-west-1.vpc.cloudera.com/s3/build/${CDH_GBN}/impala-minicluster-tarballs"
+BUILD_REPO="http://cloudera-build-us-west-1.vpc.cloudera.com/s3/build/${CDH_GBN}"
+BUILD_REPO_BASE="${BUILD_REPO}/impala-minicluster-tarballs"
 
-# Note that the Maven/Java version strings are also in impala-parent/pom.xml,
-# and version changes need to be reflected there as well. The tarballs are
-# versioned as -<GBN> whereas the Maven versions do not include a GBN, but
-# include a -SNAPSHOT (as appropriate).
-export IMPALA_HADOOP_VERSION=3.0.0-${BRANCH}-${CDH_GBN}
+if [ -z "$CAULDRON_DOCKER_PLATFORM" ]; then
+  # Outside of a Cauldron build, download CDH_PROPERTIES_FILE:
+  CDH_PROPERTIES_FILE="${IMPALA_HOME}/toolchain/cdh_components/cdh.properties.${CDH_GBN}"
+  if [ ! -f "${CDH_PROPERTIES_FILE}" ]; then
+    $WGET "${BUILD_REPO}"/cdh-properties/cdh.properties \
+      -O "${CDH_PROPERTIES_FILE}"
+  fi
+else
+  # Inside of Cauldron build, grab it from the repo
+  CDH_PROPERTIES_FILE="${CAULDRON_OUTPUT}/cdh-properties/cdh.properties"
+  [ -f "$CDH_PROPERTIES_FILE" ]
+fi
+
+# Get Java version strings from cdh.properties. Tarballs
+# append a -<GBN> to these versions, and never use -SNAPSHOT.
+# The java versions are managed in ../impala-parent/pom.xml
+# and inherit from the CDH root pom.
+export IMPALA_HADOOP_VERSION=$(cat $CDH_PROPERTIES_FILE | grep ^cdh.hadoop.version | cut -d= -f2 | sed -e s,-SNAPSHOT,,)-${CDH_GBN}
+[[ -n $IMPALA_HADOOP_VERSION ]]
 export IMPALA_HADOOP_URL=${BUILD_REPO_BASE}/hadoop-${IMPALA_HADOOP_VERSION}.tar.gz
-export IMPALA_HBASE_VERSION=2.1.0-${BRANCH}-${CDH_GBN}
+export IMPALA_HBASE_VERSION=$(cat $CDH_PROPERTIES_FILE | grep ^cdh.hbase.version | cut -d= -f2 | sed -e s,-SNAPSHOT,,)-${CDH_GBN}
+[[ -n $IMPALA_HBASE_VERSION ]]
 export IMPALA_HBASE_URL=${BUILD_REPO_BASE}/hbase-${IMPALA_HBASE_VERSION}.tar.gz
-export IMPALA_HIVE_VERSION=2.1.1-${BRANCH}-${CDH_GBN}
+export IMPALA_HIVE_VERSION=$(cat $CDH_PROPERTIES_FILE | grep ^cdh.hive.version | cut -d= -f2 | sed -e s,-SNAPSHOT,,)-${CDH_GBN}
+[[ -n $IMPALA_HIVE_VERSION ]]
+export IMPALA_KUDU_VERSION=$(cat $CDH_PROPERTIES_FILE | grep ^cdh.kudu.version | cut -d= -f2 | sed -e s,-SNAPSHOT,,)-${CDH_GBN}
 export IMPALA_HIVE_URL=${BUILD_REPO_BASE}/hive-${IMPALA_HIVE_VERSION}.tar.gz
-export IMPALA_KUDU_VERSION=1.10.0-${BRANCH}-${CDH_GBN}
+[[ -n $IMPALA_KUDU_VERSION ]]
+export IMPALA_SENTRY_VERSION=$(cat $CDH_PROPERTIES_FILE | grep ^cdh.sentry.version | cut -d= -f2 | sed -e s,-SNAPSHOT,,)-${CDH_GBN}
 export IMPALA_KUDU_URL=${BUILD_REPO_BASE}/kudu-${IMPALA_KUDU_VERSION}-'%(platform_label)'.tar.gz
-export IMPALA_SENTRY_VERSION=2.1.0-${BRANCH}-${CDH_GBN}
+[[ -n $IMPALA_SENTRY_VERSION ]]
 export IMPALA_SENTRY_URL=${BUILD_REPO_BASE}/sentry-${IMPALA_SENTRY_VERSION}.tar.gz
-export IMPALA_PARQUET_VERSION=1.9.0-${BRANCH}
-export IMPALA_AVRO_JAVA_VERSION=1.8.2-${BRANCH}
-export KUDU_JAVA_VERSION=1.10.0-${BRANCH}-SNAPSHOT
-export IMPALA_KITE_VERSION=1.0.0-${BRANCH}-SNAPSHOT
