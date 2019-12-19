3005dbf3219cfacda635d97562c0acbe1e9d16c5
norbert.luksa <norbert.luksa@cloudera.com>
2019-12-19 09:32:34 +0100

    IMPALA-8046: Support CREATE TABLE from an ORC file
    Impala supports creating a table using the schema of a file.
    However, only Parquet is supported currently. This commit adds
    support for creating tables from ORC files
    
    The change relies on the ORC Java API with version 1.5 or
    greater, because of a bug in earlier versions. Therefore, ORC is
    listed as an external dependency, instead of relying on Hive's
    ORC version (from Hive3, Hive also lists it as a dependency).
    
    Also, the commit performs a little clean-up on the ParquetHelper
    class, renaming it to ParquetSchemaExtractor and removing outdated
    comments.
    
    To create a table from an ORC file, run:
    CREATE TABLE tablename LIKE ORC '/path/to/file'
    
    Tests:
     * Added analysis tests for primitive and complex types.
     * Added e2e tests for creating tables from ORC files.
    
    Change-Id: I77cd84cda2ed86516937a67eb320fd41e3f1cf2d
    Reviewed-on: http://gerrit.cloudera.org:8080/14811
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    (cherry picked from commit 0511b44f9236c655e695185b33f412ec65a80a07)
    
    Change-Id: Ie790d7cd52f54213180af08a1efec0fcfec70be1
    Reviewed-on: https://gerrit.sjc.cloudera.com/c/cdh/impala/+/66851
    Reviewed-by: Jenkins User <jenkins@cloudera.com>
    Tested-by: Jenkins User <jenkins@cloudera.com>
    CDH-Build: zuul <zuul@cloudera.com>
    CDH-Build: Jenkins User <jenkins@cloudera.com>
    Quasar-L0: Jenkins User <jenkins@cloudera.com>

--- bin/impala-config.sh
+++ bin/impala-config.sh
@@ -192,6 +192,7 @@ export IMPALA_KITE_VERSION=1.0.0-cdh6.x-SNAPSHOT
 export IMPALA_KUDU_JAVA_VERSION=1.11.0-cdh6.x-SNAPSHOT
 export CDP_ATLAS_VERSION=2.0.0.7.1.0.0-33
 export CDP_ATLAS_BUILD_NUMBER=1352353
+export IMPALA_ORC_JAVA_VERSION=1.6.2
 
 # When IMPALA_(CDH_COMPONENT)_URL are overridden, they may contain '$(platform_label)'
 # which will be substituted for the CDH platform label in bootstrap_toolchain.py
