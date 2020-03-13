8b8d66d3e9ebe0d1adc1f1b5e5b2adedc65bd39e
Vihang Karajgaonkar <vihangk1@apache.org>
2020-03-13 15:54:38 -0700

    modified test

--- tests/custom_cluster/test_concurrent_ddls.py
+++ tests/custom_cluster/test_concurrent_ddls.py
@@ -99,41 +99,47 @@ class TestConcurrentDdls(CustomClusterTestSuite):
     def run_ddls(i):
       tbl_name = db + ".test_" + str(i)
       func_name = "f_" + str(i)
-      for query in [
-        # alter database operations
-        "comment on database %s is 'test-concurrent-ddls'" % db,
-        "alter database %s set owner user `test-user`" % db,
-        "create function %s.%s() returns int location '%s/libTestUdfs.so' \
-            symbol='NoArgs'" % (db, func_name, WAREHOUSE),
-        "drop function if exists %s.%s()" % (db, func_name),
-        # Create a partitioned and unpartitioned table
-        "create table %s (i int)" % tbl_name,
-        "create table %s_part (i int) partitioned by (j int)" % tbl_name,
-        # Below queries could fail if running with invalidate metadata concurrently
-        "alter table %s_part add partition (j=1)" % tbl_name,
-        "alter table %s_part add partition (j=2)" % tbl_name,
-        "invalidate metadata %s_part" % tbl_name,
-        "refresh %s" % tbl_name,
-        "refresh %s_part" % tbl_name,
-        "insert overwrite table %s select int_col from "
-        "functional.alltypestiny" % tbl_name,
-        "insert overwrite table %s_part partition(j=1) "
-        "values (1), (2), (3), (4), (5)" % tbl_name,
-        "insert overwrite table %s_part partition(j=2) "
-        "values (1), (2), (3), (4), (5)" % tbl_name
-      ]:
-        try:
-          # TODO(IMPALA-9123): Timeout logic here does not work for DDLs since they are
-          #  usually stuck in CREATED state and execute_async() won't return. We finally
-          #  use timeout in pytest.mark.timeout() but it's not precise. We should find a
-          #  more elegant way to detect timeout of DDLs.
-          handle = tls.client.execute_async(query)
-          is_finished = tls.client.wait_for_finished_timeout(handle, timeout=60)
-          assert is_finished, "Query timeout(60s): " + query
-          tls.client.close_query(handle)
-        except ImpalaBeeswaxException as e:
-          # Could raise exception when running with INVALIDATE METADATA
-          assert TestConcurrentDdls.is_acceptable_error(str(e), sync_ddl), str(e)
+      test_db = db + "_" + str(i)
+      try:
+        for query in [
+          "create database %s " % test_db,
+          # alter database operations
+          "comment on database %s is 'test-concurrent-ddls'" % test_db,
+          "alter database %s set owner user `test-user`" % test_db,
+          "create function %s.%s() returns int location '%s/libTestUdfs.so' \
+              symbol='NoArgs'" % (test_db, func_name, WAREHOUSE),
+          "drop function %s.%s()" % (test_db, func_name),
+          # Create a partitioned and unpartitioned table
+          "create table %s (i int)" % tbl_name,
+          "create table %s_part (i int) partitioned by (j int)" % tbl_name,
+          # Below queries could fail if running with invalidate metadata concurrently
+          "alter table %s_part add partition (j=1)" % tbl_name,
+          "alter table %s_part add partition (j=2)" % tbl_name,
+          "invalidate metadata %s_part" % tbl_name,
+          "refresh %s" % tbl_name,
+          "refresh %s_part" % tbl_name,
+          "insert overwrite table %s select int_col from "
+          "functional.alltypestiny" % tbl_name,
+          "insert overwrite table %s_part partition(j=1) "
+          "values (1), (2), (3), (4), (5)" % tbl_name,
+          "insert overwrite table %s_part partition(j=2) "
+          "values (1), (2), (3), (4), (5)" % tbl_name
+        ]:
+          try:
+            # TODO(IMPALA-9123): Timeout logic here does not work for DDLs since they are
+            #  usually stuck in CREATED state and execute_async() won't return. We finally
+            #  use timeout in pytest.mark.timeout() but it's not precise. We should find a
+            #  more elegant way to detect timeout of DDLs.
+            handle = tls.client.execute_async(query)
+            is_finished = tls.client.wait_for_finished_timeout(handle, timeout=60)
+            assert is_finished, "Query timeout(60s): " + query
+            tls.client.close_query(handle)
+          except ImpalaBeeswaxException as e:
+            # Could raise exception when running with INVALIDATE METADATA
+            assert TestConcurrentDdls.is_acceptable_error(str(e), sync_ddl), str(e)
+      finally:
+        # clean up test_db before returning
+        tls.client.execute_async("drop database if exists " + test_db)
       # TODO(IMPALA-9123): Detect hangs here instead of using pytest.mark.timeout()
       self.execute_query_expect_success(tls.client, "invalidate metadata")
 
