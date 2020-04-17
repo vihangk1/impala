c009ee650f658e183a4d696d648475f26fe715e6
Vihang Karajgaonkar <vihangk1@apache.org>
2020-04-17 10:32:47 -0700

    Implements a basic active-passive leader election based off a file
    In order to test the logic, run the test FileSystemUtil#testLeaderElection
    using mvn. The test never completes since it runs two never-ending leaderElector
    threads. In order to see what is happening look into the file in
    logs/fe_tests/org.apache.impala.common.FileSystemUtilTest-output.txt.
    
    The main idea is the leader updates the file at a faster rate than non-leaders.
    When the leader dies, non-leaders eventually realize that file is not being updated
    and hence take over. The code doesn't use local-timestamps and operates on the
    concept of duration. Needs more investigation on tolerant is this with respect
    to clock skews.
    
    the code injects random leader failures and recoveries to test fail-over.
    
    The code probably needs a thorough review to see if there are race conditions.
    
    I think we can replace the rename to a createFile with overwrite flag set to false
    provide its atomic operation accross.
    
    Change-Id: Ic6a7d708bcf5f7ac81c045a7147afe8d57334f3a

--- fe/src/test/java/org/apache/impala/common/FileSystemUtilTest.java
+++ fe/src/test/java/org/apache/impala/common/FileSystemUtilTest.java
@@ -21,9 +21,26 @@ import static org.apache.impala.common.FileSystemUtil.HIVE_TEMP_FILE_PREFIX;
 import static org.apache.impala.common.FileSystemUtil.isIgnoredDir;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -34,6 +51,183 @@ public class FileSystemUtilTest {
 
   private static final Path TEST_TABLE_PATH = new Path("/test-warehouse/foo"
       + ".db/filesystem-util-test");
+
+  @Test
+  public void testLeaderElection() throws Exception {
+    LeaderElector leaderElector1 = new LeaderElector(1, 30, 1000, "hdfs:///tmp"
+        + "/catalog_leader");
+    LeaderElector leaderElector2 = new LeaderElector(2, 30, 1000, "hdfs:///tmp"
+        + "/catalog_leader");
+    ExecutorService service = Executors.newFixedThreadPool(2);
+    Future<Void> task1 = service.submit(leaderElector1);
+    Future<Void> task2 = service.submit(leaderElector2);
+    service.shutdown();
+    task1.get();
+    task2.get();
+  }
+
+  private static class LeaderElectorException extends Exception {
+
+    LeaderElectorException(String msg, Throwable t) {
+      super(msg, t);
+    }
+  }
+
+  private static class LeaderElector implements Callable<Void> {
+
+    final long leaseDuration;
+    final long updateFrequency;
+    long pingsSinceLastChange;
+    final Path leaderFile;
+    final int id;
+    private long lastCheckedModificationTime = -1;
+    private static final Configuration CONF = new Configuration();
+    private final FileSystem fs;
+    private final AtomicBoolean isLeader = new AtomicBoolean(false);
+    private long leaderFailureInterval;
+    private static Random rand = new Random(System.currentTimeMillis());
+
+    LeaderElector(int id, long leaseDuration, long updateFrequency, String leaderFile)
+        throws IOException {
+      this.id = id;
+      this.leaseDuration = leaseDuration;
+      this.updateFrequency = updateFrequency;
+      this.leaderFile = new Path(leaderFile);
+      fs = this.leaderFile.getFileSystem(CONF);
+      // random fault injections for leaders
+      leaderFailureInterval = rand.nextInt(60) + 1;
+    }
+
+    private long getModificationTime() throws LeaderElectorException {
+      try {
+        FileStatus status = fs.getFileStatus(leaderFile);
+        return status.getModificationTime();
+      } catch (FileNotFoundException ex) {
+        return -1;
+      } catch (IOException ex) {
+        ex.printStackTrace();
+        throw new LeaderElectorException("Could not get modification time for leader "
+            + "file", ex);
+      }
+    }
+
+    private boolean createLeaderFile() throws LeaderElectorException {
+      Path tmpFileName = new Path("hdfs:///tmp/__tmp__" + id);
+      try (FSDataOutputStream out = fs
+          .create(tmpFileName, true)) {
+        out.writeBytes("leader=" + id);
+      } catch (IOException ex) {
+        throw new LeaderElectorException("Could not create temp file for leader + id",
+            ex);
+      }
+      try {
+        if (fs.exists(leaderFile)) {
+          // this thread is already the leader, we should force update the modification
+          // file
+          fs.delete(leaderFile, false);
+        }
+        // this must be atomic rename. If the leader file already exists, return false
+        //TODO can we replace this by createFile?
+        boolean ret = fs.rename(tmpFileName, leaderFile);
+        if (ret) {
+          System.out.println(System.currentTimeMillis() + " Thread " + id + " "
+              + (isLeader.get() ? "Updated " : "Created") + " leader file");
+        } else {
+          System.out.println(System.currentTimeMillis() + " Thread " + id + " "
+              + "could not create leader file");
+        }
+        return ret;
+      } catch (IOException e) {
+        throw new LeaderElectorException(e.getMessage(), e);
+      }
+    }
+
+    private boolean leaseExpired() {
+      return pingsSinceLastChange > leaseDuration;
+    }
+
+    private void reset() {
+      isLeader.set(false);
+      lastCheckedModificationTime = -1;
+      pingsSinceLastChange = 0;
+    }
+
+    private boolean isDead = false;
+    // returns true if the leader is dead, otherwise false
+    // returns false if this is not the leader
+    private boolean injectFailure() {
+      if (!isLeader.get()) return false;
+      if (leaderFailureInterval == 0) {
+        isDead = !isDead;
+        // failureInterval is complete, toggle the dead switch and return
+        System.out.println("Injecting " + (isDead? " failure " : " recovery ") + "for "
+            + "thread " + id);
+        leaderFailureInterval = rand.nextInt(60) + 1;
+        if (!isDead) {
+          // this is a recovery reset the state
+          reset();
+        }
+        return isDead;
+      }
+      leaderFailureInterval--;
+      return isDead;
+    }
+
+    @Override
+    public Void call() {
+      while (true) {
+        try {
+          if (!injectFailure()) {
+            long currentModificationTime = getModificationTime();
+            boolean fileUpdated = currentModificationTime != lastCheckedModificationTime;
+            if (fileUpdated) {
+              // file has been updated leader is alive, renew leaseExpiryCounter
+              pingsSinceLastChange = isLeader.get() ? leaseDuration/2 : 0;
+              lastCheckedModificationTime = currentModificationTime;
+            } else {
+              pingsSinceLastChange++;
+            }
+            System.out.println(System.currentTimeMillis() + " Thread " + id + " woke up. "
+                + "Leader=" + isLeader.get() + " currentModificationTime="
+                + currentModificationTime + " timeout=" + (leaseDuration - pingsSinceLastChange));
+            if (currentModificationTime == -1
+                || leaseExpired()) {
+              // either leader file is not found, or lease is expired
+              // attempt to create a leader file
+              isLeader.set(createLeaderFile());
+            }
+          }
+          Thread.sleep(updateFrequency);
+        } catch (InterruptedException ex) {
+          //
+        } catch (LeaderElectorException ex) {
+          throw new RuntimeException(ex);
+        }
+      }
+    }
+  }
+
+  private void createFileAtomically(String targetPath, long leaseRenewalDuration) {
+    Path target = new Path(targetPath);
+    FileSystem fs = null;
+    try {
+      fs = target.getFileSystem(new Configuration());
+    } catch (IOException e) {
+      e.printStackTrace();
+      Assert.fail();
+    }
+    assert (fs instanceof DistributedFileSystem);
+    try {
+      FileStatus status = fs.getFileStatus(target);
+      Assert.assertNotNull(status);
+      long modificationTime = status.getModificationTime();
+    } catch (FileNotFoundException e) {
+
+    } catch (IOException ex) {
+      ex.printStackTrace();
+      fail();
+    }
+  }
 
   @Test
   public void testIsInIgnoredDirectory() {
